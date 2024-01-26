8df0602fa650781909069d822c7ce421f70ec0d1
Edikan Ekanem <cema4.ee@gmail.com>
2024-01-26 18:23:23 -0500

    Conditional statements to check when attributes are low or high.

--- Program.cs
+++ Program.cs
@@ -14,32 +14,25 @@ namespace Assignment1
 
             string petChoice = Console.ReadLine();
 
-            //string petType;
-
             switch (petChoice)
             {
                 case "1":
-                   // petType = "Dog";
                     Console.WriteLine("\n You have chosen a Dog. What will you like to name your pet?");
                     break;
 
                 case "2":
-                   // petType = "Rabbit";
                     Console.WriteLine("\n You have chosen a Rabbit. What will you like to name your pet?");
                     break;
 
                 case "3":
-                   // petType = "Cat";
                     Console.WriteLine("\n You have chosen a Cat. What will you like to name your pet?");
                     break;
 
                 case "4":
-                   // petType = "Pig";
                     Console.WriteLine("\n You have chosen a Pig. What will you like to name your pet?");
                     break;
 
                 default:
-                   // petType = "Dog";
                     Console.WriteLine("\n Invalid pet type choice. You will be given a dog.");
                     break;
             }
@@ -105,7 +98,35 @@ namespace Assignment1
                         break;
                 }
 
-                
+                // Conditional statements to check when attributes are low or high
+                if (hunger >= 8)
+                {
+                    Console.WriteLine($"\n Opps! Hunger: {hunger}/10");
+                    Console.WriteLine($"\n {petName} is very hungry! Please feed her.");
+                    health = Math.Max(0, health - 1); // Health deteriorates if too hungry
+                }
+                else if (hunger == 0)
+                {
+                    Console.WriteLine($"\n {petName} is Full! No need to feed her.");
+                }
+                else if (happiness <= 2)
+                {
+                    Console.WriteLine($"\n {petName} is very unhappy! Play with her");
+                    health = Math.Max(0, health - 1); // Health deteriorates if too unhappy
+                }
+                else if (happiness == 10)
+                {
+                    Console.WriteLine($"\n {petName} is very happy! No need to play with her");
+                }
+                else if (health <= 3)
+                {
+                    Console.WriteLine($"\n Opps! Health: {health}/10");
+                    Console.WriteLine($"\n {petName}'s health is critical! Let her rest.");
+                }
+                else if (health == 10)
+                {
+                    Console.WriteLine($"\n {petName} is very healthy! She might want to play.");
+                }
             }
         }
     }
